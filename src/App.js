309f9f171ce5e8a3f0c33c7ba6749545c8e788a9
chloy <buffshloy55@gmail.com>
2020-05-08 13:06:49 +0200

    adding h1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React,{ useState, useEffect } from 'react';
 import './App.css';
 const api = {
   key: '4dd23b28fb57078c0d5ec9c653e203b2',
@@ -11,22 +11,26 @@ function App() {
   const [query, setQuery] = useState('');
 
   const search = (e) => {
-    if(e.key === 'Enter'){
-      fetch(`${api.url}?q=${query}&units=metric&appid=${api.key}`)
-      .then(res => res.json())
-      .then((response) => {
-        setWeather(response)
-        console.log(weather)
-        setQuery('')
-      })
+      if(e.key === 'Enter'){
+        fetch(`${api.url}?q=${query}&units=metric&appid=${api.key}`)
+        .then(res => res.json())
+        .then((response) => {
+          setWeather(response)
+          console.log(weather)
+          setQuery('')
+        })
+      }
     }
-  }
+ 
+
+
 
 
   return (
     <div className={(typeof weather.main !== 'undefined')? ((weather.main.temp > 16)? 'App warm': 'App'):
      "App"}>
       <main>
+        <h1>Weather App</h1>
         <div className='search-box'>
           <input 
             type='text' 
